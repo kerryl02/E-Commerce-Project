15fb1f00abf0596b30f9c6f48e8cfebb06084fb3
Kerryl02 <b.soda973@gmail.com>
2024-10-03 14:45:34 +0200

    :bug: fix bug

--- front/src/pages/Product.jsx
+++ front/src/pages/Product.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import {ShopContext} from '../context/ShopContext'
 import {useParams} from 'react-router-dom'
 import { assets } from '../assets/assets';
 
