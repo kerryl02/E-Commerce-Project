baf153ba83a9497e5847fcd97b7b217c87439565
Kerryl02 <b.soda973@gmail.com>
2024-10-04 05:43:50 +0200

    :sparkles: build cart system

--- front/src/pages/Product.jsx
+++ front/src/pages/Product.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState, useContext } from 'react'
 import {ShopContext} from '../context/ShopContext';
 import {useParams} from 'react-router-dom';
 import { assets } from '../assets/assets'; 
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products, currency, addToCart} = useContext(ShopContext);
   const [productData,setProductData] = useState(false);
   const [image,setImage] = useState('');
+  const [size,setSize] = useState('')
 
   const fetchProductData = async () => {
     products.map((item)=>{
@@ -50,8 +52,35 @@ const Product = () => {
               <img src={assets.star_icon} alt="" className="w-3 5" />
               <p className='pl-2'>(122)</p>
             </div>
-            <p></p>
+            <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+            <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+            <div className='flex flex-col gap-4 my-8'>
+              <p>Select Size</p>
+              <div className='flex gap-2'>
+                {productData.sizes.map((item,index)=>(
+                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                ))}
+              </div>
+            </div>
+            <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+            <hr className='mt-8 sm:w-4/5' />
+            <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+              <p>100% Original product.</p>
+              <p>Cash on delivery is available on this product.</p>
+              <p>Easy return and exchange policy within 7 days.</p>
+            </div>
         </div>
+      </div>
+      <div className='mt-20'>
+        <div className='flex'>
+              <b className='border px-5 py-3 text-sm'>Description</b>
+              <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions whithout the need for a physical presence. E-commerce website have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
+          <p>E-commerce websites typically display products or services along with detailed description, image, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information</p>
+        </div>
+        <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
       </div>
     </div>
   ) : <div className='opacity-0'></div>
