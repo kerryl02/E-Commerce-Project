25b73f19882f075222ea19914e74016daaf44762
Kerryl02 <b.soda973@gmail.com>
2024-10-07 02:08:25 +0200

    :sparkles: add admin add and remove feature

--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -5,12 +5,12 @@ const addProduct = async (req,res) => {
     try {
 
         const {name,description,price,category,subCategory,sizes,bestseller} = req.body;
-        const image1 = req.files.image1 ? req.files.image1[0] : null;
-        const image2 = req.files.image2 ? req.files.image2[0] : null;
-        const image3 = req.files.image3 ? req.files.image3[0] : null;
-        const image4 = req.files.image4 ? req.files.image4[0] : null;
+        const image1 = req.files?.image1 ? req.files.image1[0] : null;
+        const image2 = req.files?.image2 ? req.files.image2[0] : null;
+        const image3 = req.files?.image3 ? req.files.image3[0] : null;
+        const image4 = req.files?.image4 ? req.files.image4[0] : null;
 
-        const images = [image1,image2,image3,image4].filter((item)=>item !== undefined);
+        const images = [image1,image2,image3,image4].filter((item)=>item !== null);
 
         let imagesUrl = await Promise.all(
             images.map(async (item) => {
